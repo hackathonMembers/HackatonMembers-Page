0b3519ae2c6b61e34373d2616fe39250807e314c
Nicole Alves Brand <131276280+nicoleabrand@users.noreply.github.com>
2024-07-29 22:29:58 -0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -9,10 +9,6 @@ const router = createRouter({
       name: 'equipe',
       component: () => import('../views/EquipeView.vue')
     },{
-      path: '/cargos',
-      name: 'cargos',
-    component: () => import('../views/CargosView.vue')
-    },{
       path: '/membro:nome',
       name: 'membro',
     component: () => import('../views/MembroView.vue'),
